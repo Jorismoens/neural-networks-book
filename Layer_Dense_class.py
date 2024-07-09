661274284f24ba024704e0aaf33f6fe82052aeff
Jorismoens <72558033+Jorismoens@users.noreply.github.com>
2024-07-09 11:45:40 +0200

    feature: add backward pass to dense layer and ReLU activation to compute their derivatives for back-propagation

--- Layer_Dense_class.py
+++ Layer_Dense_class.py
@@ -1,11 +1,26 @@
 import numpy as np
 
-# Create a dense layer with n_inputs inputs and n_neurons neurons. Randomly initialize weights and biases
+# Create a dense layer with n_inputs inputs and n_neurons neurons.
+# Randomly initialize weights and biases
 class Layer_Dense:
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.5 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.inputs = None
+        self.output = None
+        self.dinputs = None
+        self.dweights = None
+        self.dbiases = None
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
--- activation_ReLU_class.py
+++ activation_ReLU_class.py
@@ -3,5 +3,19 @@ import numpy as np
 
 class Activation_ReLU:
 
+    def __init__(self):
+        self.output = None
+        self.inputs = None
+        self.dinputs = None
+
     def forward(self, inputs):
+        # Remember input values
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Derivative of ReLU is 0 before activation, slope is 1 after activation
+        # Multiply with result up to now
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
