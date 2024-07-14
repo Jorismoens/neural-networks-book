0029475aebe8084a7262ffba329337c4d124dc67
Jorismoens <72558033+Jorismoens@users.noreply.github.com>
2024-07-14 11:12:35 +0200

    feature: add derivative for Softmax activation function to its backward call

--- Activation_Softmax_class.py
+++ Activation_Softmax_class.py
@@ -4,6 +4,7 @@ import numpy as np
 class Activation_Softmax:
 
     def __init__(self):
+        self.dinputs = None
         self.output = None
 
     def forward(self, inputs):
@@ -15,3 +16,20 @@ class Activation_Softmax:
                                             keepdims=True)
 
         self.output = probabilities
+
+    # Derivative of Softmax function
+    # dS_ij/dz_ik = S_ij * delta_jk - S_ij * S_ik
+    # With delta_jk the Kronecker delta
+    def backward(self, dvalues):
+
+        # TODO instead of line below from NN book, dinputs is declared in init, check if this compiles
+        # self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
